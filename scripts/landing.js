207ee98b3a2f9b84837ea43cd6d62622dc2c3f6c
Raynier Hebron <raynierhebron@gmail.com>
2018-01-15 17:13:49 -0800

    dom-scripting-event-assignment-redo

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,20 +1,24 @@
-var animatePoints = function() {
-     var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
-     var revealPoint = function(index){
-         points[index].style.opacity = 1;
-         points[index].style.transform = "scaleX(1) translateY(0)";
-         points[index].style.msTransform = "scaleX(1) translateY(0)";
-         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-     }
+var revealPoint = function(point){
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
-     for (var i = 0; i < points.length; i++) {
-          revealPoint(i);
-     }
+var animatePoints = function(points) {
+    forEach(points, revealPoint)
  };
 
  window.onload = function() {
-   window.addEventListener('scroll', function(event) {
-       console.log(event);
-   });
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+
+     window.addEventListener("scroll", function(event) {
+         if (pointsArray[0].getBoundingClientRect().top <= 500) {
+             animatePoints(pointsArray);
+         }
+      });
 }
